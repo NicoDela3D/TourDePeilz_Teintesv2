568ada2804038243532af145a5854611c32cdc40
NicoDela3D <info@nicodela.com>
2024-08-05 20:54:22 +0200

    Changs chnages changes

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ window.onload = function () {
     const baseImage = new Image();
     const maskImage = new Image();
 
-    const MAX_WIDTH = 800;
-    const MAX_HEIGHT = 600;
+    const MAX_WIDTH = 1920;
+    const MAX_HEIGHT = 1080;
 
     baseImage.crossOrigin = "Anonymous";
     maskImage.crossOrigin = "Anonymous";
@@ -86,7 +86,7 @@ window.onload = function () {
         }
 
         maskCtx.putImageData(imageData, 0, 0);
-        mainCtx.globalCompositeOperation = 'screen'; // Use multiply blend mode
+        mainCtx.globalCompositeOperation = 'overlay'; // Use multiply blend mode
         mainCtx.drawImage(maskCanvas, 0, 0);
         mainCtx.globalCompositeOperation = 'source-over'; // Reset blend mode to default
     }
